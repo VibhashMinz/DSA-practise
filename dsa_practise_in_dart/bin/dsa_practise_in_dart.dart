b5a4ac281a9108990ab59cb4255e3e033aa1f6c5
vibhash <vibhashgyanminz23@gmail.com>
2025-10-07 15:32:45 +0530

    Add main entry point and array solutions with tests

--- dsa_practise_in_dart/bin/dsa_practise_in_dart.dart
+++ dsa_practise_in_dart/bin/dsa_practise_in_dart.dart
@@ -1,5 +1,11 @@
+import 'package:dsa_practise_in_dart/array/find_max.dart';
+import 'package:dsa_practise_in_dart/array/find_second_largest.dart';
 import 'package:dsa_practise_in_dart/dsa_practise_in_dart.dart' as dsa_practise_in_dart;
 
 void main(List<String> arguments) {
+  List<int> collection = [-12, 18, 0, 25, 91, -56, 79, 222, 222, 100, -102];
   print('Hello world: ${dsa_practise_in_dart.calculate()}!');
+  print(findMax(collection));
+  print(findSecondLargest(collection));
+  print(findSecondLargestElement(collection));
 }
